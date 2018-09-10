5393e4fda3fd5fa8e7a67cd8e28cfb6c840ed248
colesayer <colesayershapiro@gmail.com>
2018-09-10 14:27:47 -0400

    fixed scroll buttons

--- src/components/nav/sidelinksPlaceholder.js
+++ src/components/nav/sidelinksPlaceholder.js
@@ -6,11 +6,12 @@ class SideLinksPlaceholder extends Component{
     return(
       <div className="side-links-placeholder" style={{width: this.props.windowWidth, height: this.props.windowHeight}}>
         <SideLinks />
-        <div
-          style={!this.props.isTitle ? {visibility: 'visible', opacity: 1, transition: 'opacity 2s linear'} : {visibility: 'hidden', opacity: 0, transition: 'opacity 2s linear'}}
-          className="scroll-up-button"
-          onClick={this.props.handleClick}
-          >
+        <div className="scroll-up-button-container" onClick={this.props.handleClick}>
+          <div
+            style={!this.props.isTitle ? {visibility: 'visible', opacity: 1, transition: 'opacity 2s linear'} : {visibility: 'hidden', opacity: 0, transition: 'opacity 2s linear'}}
+            className="scroll-up-button"
+            >
+          </div>
         </div>
       </div>
     )
--- src/components/three/threePlaceholder.js
+++ src/components/three/threePlaceholder.js
@@ -6,10 +6,11 @@ class ThreePlaceholder extends Component{
     return(
       <div className="three-placeholder" style={{width: '100%', height: '50%'}}>
         <ThreeTitle/>
-        <div
-          style={this.props.isTitle ? {visibility: 'visible', opacity: 1, transition: 'opacity 2s linear'} : {visibility: 'hidden', opacity: 0, transition: 'opacity 2s linear'}}
-          onClick={this.props.handleClick}
-          className="scroll-down-button">
+        <div className="scroll-down-button-container" onClick={this.props.handleClick}>
+          <div
+            style={this.props.isTitle ? {visibility: 'visible', opacity: 1, transition: 'opacity 2s linear'} : {visibility: 'hidden', opacity: 0, transition: 'opacity 2s linear'}}
+            className="scroll-down-button">
+          </div>
         </div>
       </div>
     )
